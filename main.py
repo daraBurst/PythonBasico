416b114899afe91b0cad4d153483681eba5f7604
Rafael Meza <raafael.0291@gmail.com>
2020-09-07 14:50:51 -0600

    Second Comment
    Clase_08_09_2020

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Hello World")
+
+#Este es un comentario de una linea
+
+'''
+Este es un
+comentario
+multi-linea 
+
+'''
+
+#Python bi tiene comando para declarar una variable
+#comillas simples o dobles para declar una variable de tipo cadena
+
+x = 5
+y = "Hello Word"
+
+print(x)
+print(y)
